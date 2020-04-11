12799cae0de1a77647d0abc31f86d1eb071d925a
zhangll6 <zhangll6@ziroom.com>
2020-04-11 12:43:19 +0800

    eureka

--- src/main/java/com/example/cloud/democloud/DemoCloudApplication.java
+++ src/main/java/com/example/cloud/democloud/DemoCloudApplication.java
@@ -2,8 +2,10 @@ package com.example.cloud.democloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoCloudApplication {
 
 	public static void main(String[] args) {
