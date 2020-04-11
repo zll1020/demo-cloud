298d0ccb69e287eb42d49cf50c7d455814f70ee7
zhangll6 <zhangll6@ziroom.com>
2020-04-11 12:04:10 +0800

    服务消费者 删除 与 actuator

--- src/main/java/com/example/cloud/democloud/controller/UserController.java
+++ src/main/java/com/example/cloud/democloud/controller/UserController.java
@@ -1,28 +1,29 @@
-package com.example.cloud.democloud.controller.movie;
+package com.example.cloud.democloud.controller;
 
+import com.example.cloud.democloud.dao.UserRespository;
 import com.example.cloud.democloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Description:
  * User: zhangll
  * Date: 2020-04-11
- * Time: 11:12
+ * Time: 11:00
  */
 @RestController
-@RequestMapping("movie")
-public class MovieController {
+@RequestMapping("/user")
+public class UserController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserRespository userRespository;
 
-    @GetMapping(("/getUser/{id}"))
+    @GetMapping("/{id}")
     public User findById(@PathVariable Long id){
-        return this.restTemplate.getForObject("http://localhost:8000/user/" + id,User.class);
+        User user = this.userRespository.getOne(id);
+        return user;
     }
 }
