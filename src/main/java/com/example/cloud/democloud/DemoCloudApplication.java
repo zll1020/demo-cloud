7ef70ffa8b1ac772c108495018a2cb18c786ec50
zhangll6 <zhangll6@ziroom.com>
2020-04-11 10:20:02 +0800

    Create project

---
+++ src/main/java/com/example/cloud/democloud/DemoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.democloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloud/democloud/DemoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.democloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
